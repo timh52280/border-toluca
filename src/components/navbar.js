b798c1cacfea5d300fc50600533133edadec594e
timh52280 <timh52280@gmail.com>
2022-07-12 08:58:20 -0500

    Se corrige background

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,5 @@
+import * as React from "react"
+
 const Navbar = () => (
 <nav class="menu">
   <ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+import Navbar from "../components/navbar"
 
 const links = [
   {
